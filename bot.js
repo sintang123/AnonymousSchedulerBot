bbdb734522a39d2bdf4796587042a07cacf36d69
sintang123 <35473953+sintang123@users.noreply.github.com>
2018-02-05 14:51:29 +0800

    Fix error

--- bot.js
+++ bot.js
@@ -26,6 +26,7 @@ var requesterId = "";
 var yesCount = 0;
 var noCount = 0;
 
+var realRequesterId = "";
 
 var bannedEmail1 = "spark-cisco-it-admin-bot@cisco.com";
 var bannedEmail2 = "happyscheduler@sparkbot.io";
@@ -80,8 +81,8 @@ controller.hears('-start *', ['direct_message','direct_mention','mention'], func
     //var toJson = JSON.parse(message);
     var rp = require('request-promise');
 
-    //requesterId = message.raw_message.data.personId;;
-    requesterId = " ";
+    realRequesterId = message.raw_message.data.personId;;
+    requesterId = message.raw_message.data.personId;
 
     activityDetails = message.text.substr(message.text.indexOf(" ") + 1);
 
@@ -101,7 +102,7 @@ controller.hears('-start *', ['direct_message','direct_mention','mention'], func
     rp(options)
         .then(function (parsedBody){
            var tempJson = JSON.parse(parsedBody);
-           console.log(tempJson);
+           
            for (var i = 0; i < tempJson.items.length; i++) {
 
              var member = tempJson.items[i];
@@ -110,11 +111,9 @@ controller.hears('-start *', ['direct_message','direct_mention','mention'], func
              //if (member.personEmail != bannedEmail1 && member.personEmail != bannedEmail2 &&  (member.personEmail.indexOf("sparkbot.io") == -1)){
                 memberIDList.push(member.personId);
                 memberNameList.push(member.personDisplayName);
-                console.log("Person ID " + (i+1) + " : " + member.personId);
-
-
+                
                 // Sending Personal Messages
-                console.log("Sending votes..");
+                console.log("Sending feedback request to members in the room ...");
 
                var options3 = { method: 'POST',
                     url: 'https://api.ciscospark.com/v1/messages',
@@ -124,13 +123,14 @@ controller.hears('-start *', ['direct_message','direct_mention','mention'], func
                        Authorization: 'Bearer ZWQ5MDA3ZWItYWJmYi00NTY3LWFhMmUtNzI4NWFmNjFkZGU1MzA0ZWU0MWUtMmVk',
                       'Content-type': 'application/json; charset=utf-8' },
                       body: '{\n\t"toPersonId": "' + member.personId + '",\n\t"text": "Vote in Progress. \\n \\n' + activityDetails + ' \\n\\nPlease enter -yes or -no [suggestions?] "\n}' };
-                      console.log(options);
+                      
                 rp(options3)
                 .then(function (parsedBody){
+                    console.log("Feedback request sent to all! ");
                     
 
                 }).catch(function (err){
-                        console.log(err);
+                      console.log(err);
                 });
 
 
@@ -138,6 +138,7 @@ controller.hears('-start *', ['direct_message','direct_mention','mention'], func
 
            }
            bot.reply(message, "Voting have begun. I will notify you when all votes have been collected. \n\nIf you wish to stop collecting votes and see the results, say **-quit**");
+           
            createWebHooks();
            
         }).catch(function (err){
@@ -150,7 +151,7 @@ controller.hears('-start *', ['direct_message','direct_mention','mention'], func
 
 function createWebHooks(){
     var rp = require('request-promise');
-    console.log("Creating WebHooks..");
+    console.log("Creating Webhooks for each member in the room ..");
 
     for (var z = 0; z < memberIDList.length; z++){
 
@@ -163,18 +164,21 @@ function createWebHooks(){
               'Content-type': 'application/json; charset=utf-8' },
               body: '{\n\t"name": "' + memberNameList[z] + '",\n\t"targetUrl": "https://yuenyuen.localtunnel.me",\n\t"resource": "messages",\n\t"event": "created",\n\t"filter": "personId=' + memberIDList[z] + '"\n}' };
     
-    console.log(options2);
     rp(options2)
         .then(function (parsedBody){
+            console.log("Webhooks for " + memberNameList[z] + " created.");
             var tempJson = JSON.parse(parsedBody);
             webHookList.push(tempJson.id);
 
 
         }).catch(function (err){
-                console.log(err);
+            console.log("Error in creating Webhook for " + memberNameList[z]);
+            console.log(err);
     });
     
     }
+
+    console.log("Webhook creation process completed.");
 
 }
 
@@ -184,14 +188,14 @@ function createWebHooks(){
 controller.hears('-yes *', ['direct_message','direct_mention','mention'], function (bot, message) {
     //var toJson = JSON.parse(message);
     var rp = require('request-promise');
-
-
     var tempId = message.raw_message.data.personId;
     var index = memberIDList.indexOf(tempId);
     var webHookId = webHookList[index];
 
     console.log(memberNameList[index] + " have voted YES");
+
     console.log("Deleting WebHook for " + memberNameList[index]);
+
     var options = { method: 'DELETE',
      url: 'https://api.ciscospark.com/v1/webhooks/' + webHookId,
     headers: 
@@ -202,16 +206,22 @@ controller.hears('-yes *', ['direct_message','direct_mention','mention'], functi
 
     rp(options)
         .then(function (parsedBody){
-           yesCount  = yesCount + 1;
            console.log(memberNameList[index] + "'s Webhook deleted");
+           yesCount  = yesCount + 1;
+           
 
            if((memberIDList.length - (yesCount + noCount)) > 0){
+
+              // Not All Have Answered            
              checkVotingStatus(false);
            }else{
+
+              // All Answered
               checkVotingStatus(true);
            }
 
         }).catch(function (err){
+            console.log("Error in deleting Webhook for " + memberNameList[index]);
             console.log(err);
         });
 
@@ -222,7 +232,7 @@ controller.hears('-no *', ['direct_message','direct_mention','mention'], functio
     //var rp = require('request-promise');
 
    var alternatives = message.text.substr(message.text.indexOf(" ") + 1);
-    console.log("ALT : " + alternatives);
+   
    if (alternatives.length > 1 && alternatives != '-no'){
       alternativesList.push(alternatives);
     }
@@ -235,6 +245,7 @@ controller.hears('-no *', ['direct_message','direct_mention','mention'], functio
 
     console.log(memberNameList[index] + " have voted NO");
     console.log("Deleting WebHook for " + memberNameList[index]);
+
     var options = { method: 'DELETE',
      url: 'https://api.ciscospark.com/v1/webhooks/' + webHookId,
     headers: 
@@ -249,25 +260,36 @@ controller.hears('-no *', ['direct_message','direct_mention','mention'], functio
            console.log(memberNameList[index] + "'s Webhook deleted");
 
            if((memberIDList.length - (yesCount + noCount)) > 0){
+
+            // Not All Have Answered
              checkVotingStatus(false);
            }else{
+
+            // All Answered
               checkVotingStatus(true);
            }
 
         }).catch(function (err){
+            console.log("Error in deleting Webhook for " + memberNameList[index]);
             console.log(err);
         });
 
 });
 
 function checkVotingStatus(forcedQuit){
+    
+    console.log("Checking Voting Status ... ");
 
-    console.log("Checking Voting Status..");
-    var rp = require('request-promise');
+    var rp1 = require('request-promise');
+    var rp2 = require('request-promise');
+    var rp3 = require('request-promise');
     var curCount = yesCount + noCount;
 
     
     if((memberIDList.length - curCount) > 0 || forcedQuit == false){
+
+        console.log("Not All Have Replied... Posting number of remaining responses required ..");
+
         var options3 = { method: 'POST',
             url: 'https://api.ciscospark.com/v1/messages',
             headers: 
@@ -277,50 +299,65 @@ function checkVotingStatus(forcedQuit){
               'Content-type': 'application/json; charset=utf-8' },
               body: '{\n\t"roomId": "' + spaceId + '",\n\t"text": "' + (memberIDList.length - curCount) + ' responses remaining"\n}' };
 
-      rp(options3)
+      rp1(options3)
         .then(function (parsedBody){
            
            console.log((memberIDList.length - curCount) + " votes left.")
 
         }).catch(function (err){
+            console.log("Error in posting number of remaining votes required.");
             console.log(err);
         });
 
     } else{
-         var options3 = { method: 'POST',
-            url: 'https://api.ciscospark.com/v1/messages',
-            headers: 
-             { 'Postman-Token': '39ba5087-93c0-481e-373c-156d871943af',
-              'Cache-Control': 'no-cache',
-               Authorization: 'Bearer ZWQ5MDA3ZWItYWJmYi00NTY3LWFhMmUtNzI4NWFmNjFkZGU1MzA0ZWU0MWUtMmVk',
-              'Content-type': 'application/json; charset=utf-8' },
-              body: '{\n\t"roomId": "' + spaceId + '",\n\t"text": "Voting completed! ' + yesCount + ' says YES! and ' + noCount + ' have said NO! \n}' };
+
+         console.log("Either All Have Replied...  OR  -Quit ");
+         console.log("Posting the results of the vote ..");
+
+       
+
+var options3 = { method: 'POST',
+  url: 'https://api.ciscospark.com/v1/messages',
+  headers: 
+   { 'Postman-Token': '1ef4b933-0ab0-a548-27f9-86bb8b330e22',
+     'Cache-Control': 'no-cache',
+     'Content-Type': 'application/json; charset=utf-8',
+     Authorization: 'Bearer ZWQ5MDA3ZWItYWJmYi00NTY3LWFhMmUtNzI4NWFmNjFkZGU1MzA0ZWU0MWUtMmVk' },
+  body: '{\n\t"roomId": "' + spaceId + '",\n\t"text": "Voting Completed! ' + yesCount + ' have said YES! and ' + noCount + ' have said NO! "\n}' };
 
 
 
-
-          rp(options3)
+          rp1(options3)
             .then(function (parsedBody){
                
-               console.log("No: Feedback : " + alternativesList.length);
+               console.log("Posted results of the vote");
+               console.log("Total Feedback Recevied : " + alternativesList.length);
 
                
                // Inform Requester of any Suggestion
 
                var feedback = "";
                if (alternativesList.length > 0){
-                feedback = "Vote have ended. Here are the feedback received from your team.\\n\\n";
+                  console.log(alternativesList.length + " feedback collected. Compiling it now.. ");
+
+
+                 feedback = "Vote have ended. Here are the feedback received from your team.\\n\\n";
+
                   for (var h = 0 ; h < alternativesList.length; h++){
+                    console.log("Feedback " + (h+1) + " : " + alternativesList[h]);
                    feedback = feedback + "Feedback " + (h + 1) + " :\\n" + alternativesList[h] + " \\n \\n";
 
                  }
-                 console.log(feedback);
+                 console.log("Total Feedback : " + feedback);
               }else{
+
+                  console.log(alternativesList.length + " feedback collected. No Feedback received");
                  feedback = "Vote have ended. No feedback received from your team.\\n";
 
               }
                
-
+               console.log("Preparing to inform administrator of the feedback received from team..");
+/*
               var options4 = { method: 'POST',
                       url: 'https://api.ciscospark.com/v1/messages',
                       headers: 
@@ -329,14 +366,24 @@ function checkVotingStatus(forcedQuit){
                          Authorization: 'Bearer ZWQ5MDA3ZWItYWJmYi00NTY3LWFhMmUtNzI4NWFmNjFkZGU1MzA0ZWU0MWUtMmVk',
                         'Content-type': 'application/json; charset=utf-8' },
                         body: '{\n\t"toPersonId": "' + requesterId + '",\n\t"text": "' + feedback + '"\n}' };
+*/
+var options4 = { method: 'POST',
+  url: 'https://api.ciscospark.com/v1/messages',
+  headers: 
+   { 'Postman-Token': '1ef4b933-0ab0-a548-27f9-86bb8b330e22',
+     'Cache-Control': 'no-cache',
+     'Content-Type': 'application/json; charset=utf-8',
+     Authorization: 'Bearer ZWQ5MDA3ZWItYWJmYi00NTY3LWFhMmUtNzI4NWFmNjFkZGU1MzA0ZWU0MWUtMmVk' },
+  body: '{\n\t"toPersonId": "' + requesterId + '",\n\t"text": "' + feedback + '"\n}' };
 
-                rp(options4)
+                rp2(options4)
                   .then(function (parsedBody){
-                     
-                       console.log("Deleting Existing Webhooks..");
+                        console.log("Posted to admin of the results of the feedback..");
+
+                       console.log("Deleting Remaining Webhooks - CheckVotingStatus");
                         for(var z = 0; z < webHookList.length; z++){
                             
-                            console.log("Deleting Webhook..");
+                            console.log("Deleting Webhook ..");
                             var options = { method: 'DELETE',
                             url: 'https://api.ciscospark.com/v1/webhooks/' + webHookList[z],
                             headers: 
@@ -345,10 +392,11 @@ function checkVotingStatus(forcedQuit){
                                Authorization: 'Bearer ZWQ5MDA3ZWItYWJmYi00NTY3LWFhMmUtNzI4NWFmNjFkZGU1MzA0ZWU0MWUtMmVk',
                               'Content-type': 'application/json; charset=utf-8' } };
 
-                            rp(options)
+                            rp3(options)
                                 .then(function (parsedBody){
                                   console.log(webHookList.length +  " Webhook remaining..");
                                 }).catch(function (err){
+                                  console.log("Error in deleting Webhook - CheckVotingStatus ");
                                     console.log(err);
                                 });
 
@@ -381,6 +429,7 @@ function checkVotingStatus(forcedQuit){
 
 
             }).catch(function (err){
+              console.log("Error in posting the results of the vote. - CheckVotingStatus");
                 console.log(err);
             });
 
@@ -395,6 +444,8 @@ function checkVotingStatus(forcedQuit){
 }
 
 controller.hears('-quit', ['direct_message','direct_mention','mention'], function (bot, message) {
+  console.log("alternatives..");
+  console.log(alternativesList);
     var rp = require('request-promise');
     var myId = requesterId;
     //curCount = memberIDList.length;
@@ -428,12 +479,14 @@ controller.hears('-quit', ['direct_message','direct_mention','mention'], functio
             // Inform Requester of any Suggestion
 
                var feedback = "";
+               console.log("Length of Alt List : " + alternativesList.length);
                if (alternativesList.length > 0){
                 feedback = "Vote have ended. Here are the feedback received from your team.\\n\\n";
                   for (var h = 0 ; h < alternativesList.length; h++){
                    feedback = feedback + "Feedback " + (h + 1) + " :\\n" + alternativesList[h] + " \\n \\n ";
 
                  }
+                 console.log("Total Feedback :");
                  console.log(feedback);
               }else{
                  feedback = "Vote have ended. No feedback received from your team.\\n";
@@ -448,7 +501,7 @@ controller.hears('-quit', ['direct_message','direct_mention','mention'], functio
                         'Cache-Control': 'no-cache',
                          Authorization: 'Bearer ZWQ5MDA3ZWItYWJmYi00NTY3LWFhMmUtNzI4NWFmNjFkZGU1MzA0ZWU0MWUtMmVk',
                         'Content-type': 'application/json; charset=utf-8' },
-                        body: '{\n\t"toPersonId": "' + myId + '" ,\n\t"text": "' + feedback + '"\n}' };
+                        body: '{\n\t"toPersonId": "' + myId + '" ,\n\t"text": "sad"\n}' };
 
 
               console.log("HeyHEY : " + options4.headers.body);
@@ -480,11 +533,13 @@ controller.hears('-quit', ['direct_message','direct_mention','mention'], functio
 
 
                   }).catch(function (err){
+                      console.log("Error in option 4");
                       console.log(err);
                   });
 
 
         }).catch(function (err){
+            console.log("Error in option 3");
             console.log(err);
         });
 
